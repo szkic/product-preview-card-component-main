0e975b5e7f9d5fa6bd0dc97d99ec14ecd1a0fe45
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-02-06 21:14:45 +0100

    hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     colors: {
       cyan: "hsl(158, 36%, 37%)",
+      darkCyan: "#1A4032",
       cream: "hsl(30, 38%, 92%)",
       darkBlue: "hsl(212, 21%, 14%)",
       grayishBlue: "hsl(228, 12%, 48%)",
